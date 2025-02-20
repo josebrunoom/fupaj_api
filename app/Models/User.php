070e996ee12e4c5c3f2925862a38a5c3adcdde21
josebrunoom <josebrunoom@gmail.com>
2025-02-20 15:24:16 -0300

    custom claims

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,12 @@ class User extends Authenticatable implements JWTSubject
     }
 
     public function getJWTCustomClaims() {
-        return [];
+        return [
+            'usr'=> [
+                'nome' => $this->nome,
+                'email' => $this->email,
+                'role' => $this->role
+            ]
+        ];
     }
 }
