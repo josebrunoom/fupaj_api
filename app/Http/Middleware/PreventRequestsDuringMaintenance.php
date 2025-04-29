13ff23ef30aff855cc0631a3fc89d79d6c0dd48b
josebrunoom <josebrunoom@gmail.com>
2025-04-29 11:00:39 -0300

    refactor: implementa versão customizada do middleware PreventRequestsDuringMaintenance

--- app/Http/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -2,16 +2,74 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
-class PreventRequestsDuringMaintenance extends Middleware
+class PreventRequestsDuringMaintenance
 {
     /**
      * The URIs that should be reachable while maintenance mode is enabled.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $except = [
         //
     ];
+
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->app = App::getInstance();
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($this->app->isDownForMaintenance() && !$this->isPassable($request)) {
+            throw new \Symfony\Component\HttpKernel\Exception\HttpException(503);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Determine if the request has a URI that should be allowed to pass through maintenance mode.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function isPassable(Request $request)
+    {
+        foreach ($this->except as $except) {
+            if ($except !== '/') {
+                $except = trim($except, '/');
+            }
+
+            if ($request->is($except)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
